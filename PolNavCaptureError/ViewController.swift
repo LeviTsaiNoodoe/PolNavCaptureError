43f9a902f578bfc9ba750464e61f6a68848f08ca
levitsai <levitsai@noodoe.com>
2018-11-16 14:16:56 +0800

    commit

--- PolNavCaptureError/ViewController.swift
+++ PolNavCaptureError/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import PolnavFramework
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        PolnavManager.install(with: self)
+        
+        
+        PolnavManager.default.loadPolnavMobileFolder(product: ["Taiwan_RM" : Date().timeIntervalSinceNow], displacement: 150)
+        
+        
+        PolnavManager.default.updateProductMapVoiceInfo()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +34,8 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: PolnavManagerDataSource{
+    func noodoeUUID() -> String {
+        return "RFBSR30JDJB100057"
+    }
+}
